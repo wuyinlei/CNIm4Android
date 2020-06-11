88875aee1d42d23770c665d14951064389f5a050
若兰明月 <wuyinlei19931118@hotmail.com>
2020-06-11 14:18:51 +0800

    Update UploadHelper.java

--- factory/src/main/java/com/mingchu/factory/net/UploadHelper.java
+++ factory/src/main/java/com/mingchu/factory/net/UploadHelper.java
@@ -28,9 +28,9 @@ public class UploadHelper {
     private static final String TAG = UploadHelper.class.getName();
 
     //APP_KEY
-    private static final String ACCESS_KEY_ID = "LTAImUdBwv9uCm0s";
+    private static final String ACCESS_KEY_ID = "xxxxxxx";
     //KEY_SECRET
-    private static final String ACCESS_KEY_SECRET = "3LzVjvZVbad14UziBKux1gsEnxugMn";
+    private static final String ACCESS_KEY_SECRET = "xxxxxxxx";
 
     //终结点  与我们自己的申请的存储区域有关系
     public static final String ENDPOINT = "http://oss-cn-shanghai.aliyuncs.com";
